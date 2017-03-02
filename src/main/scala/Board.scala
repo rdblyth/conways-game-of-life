9106e28448a6305f2a5e1c67ca1096984c05e6a5
Daneil Blyth <rdblyth@gmail.com>
2017-03-01 19:36:30 -0500

    Refactored code.

--- src/main/scala/Board.scala
+++ src/main/scala/Board.scala
@@ -1,20 +1,20 @@
 case class Board(aliveCells: Set[Cell]) {
 
-  def tick() : Board = new Board(getAliveCellsAfterTick())
+  def tick() : Board = new Board(aliveCellsAfterTick())
 
   def isAlive(cell:Cell) : Boolean = aliveCells.contains(cell)
 
-  private def getAliveCellsAfterTick() = getAliveCellsWithNeighbors().filter(isAliveAfterTick)
+  private def aliveCellsAfterTick() = aliveCellsWithNeighbors().filter(isAliveAfterTick)
 
   private def isAliveAfterTick(cell:Cell)  = {
-    (getNumberOfAliveNeighbors(cell), isAlive(cell)) match {
+    (numAliveNeighbors(cell), isAlive(cell)) match {
       case (2, true) => true
       case (3, _) => true
       case _ => false
     }
   }
 
-  private def getAliveCellsWithNeighbors() = aliveCells ++ aliveCells.flatMap(_.neighbors)
+  private def aliveCellsWithNeighbors() = aliveCells ++ aliveCells.flatMap(_.neighbors)
 
-  private def getNumberOfAliveNeighbors(cell:Cell) = cell.neighbors.filter(isAlive).length
+  private def numAliveNeighbors(cell:Cell) = cell.neighbors.filter(isAlive).length
 }
