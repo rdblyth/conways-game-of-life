cd8d3960b9489c06cf46c94dee8db9ed438b518d
Daneil Blyth <rdblyth@gmail.com>
2017-03-01 19:29:20 -0500

    Removed the use of MutableList

--- src/main/scala/Cell.scala
+++ src/main/scala/Cell.scala
@@ -1,12 +1,9 @@
-import scala.collection.mutable.MutableList
-
 case class Cell(x:Int, y:Int) {
 
-  def neighbors : List[Cell] = {
-    val neighbors = MutableList[Cell]()
-    for(i <- -1 to 1;
-        j <- -1 to 1) {
-      neighbors +=  Cell(x+i, y+j)
+  lazy val neighbors : List[Cell] = {
+    val neighbors = {
+      for(i <- -1 to 1;
+          j <- -1 to 1) yield Cell(x+i, y+j)
     }
     neighbors.filter(cell => cell != this).toList
   }
